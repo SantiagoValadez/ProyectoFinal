8868ad5a6568b213b3d99ababe69d03e6a817432
saniagoValadez <saniago1340@gmail.com>
2024-02-29 19:57:51 -0600

    Se hicieron correciones y se dio funcionalidad con JS

---
+++ experiencia/app.js
@@ -0,0 +1,36 @@
+// PRIMER INTENTO DE LIGHTBOXES
+// const images = document.querySelectorAll('.container #img');
+// const containerImage = document.querySelector('.container-img')
+// const imageContainer = document.querySelector('.img-show')
+
+const lightbox = require("lightbox2")
+
+// images.forEach(image =>{
+//   image.addEventListener('click', ()=>{
+//     addImage(image.getAttribute('src'),image.getAttribute('alt'))
+
+//   })
+// })
+
+// const addImage = (src, alt) =>{
+//   containerImage.classList.toggle('move')
+//   imageContainer.src = src;
+// }
+
+// containerImage.addEventListener('click', ()=>{
+//   containerImage.classList.toggle('move');
+// })
+// PRIMER INTENTO DE LIGHTBOXES
+
+
+
+// PLUGGIN lightbox
+lightbox.option({
+  'resizeDuration': 200,
+  'wrapAround': true
+})
+// PLUGGIN lightbox
+
+
+
+
